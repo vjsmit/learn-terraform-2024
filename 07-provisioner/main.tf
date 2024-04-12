d93a282470559e591cdb45e14ff904f0fb7c21c3
smvijay <vj.smit97@gmail.com>
2024-04-12 13:42:23 -0400

    resource

---
+++ 07-provisioner/main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-06455962b48979e39"]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    inline = [
+      "sudo labauto ansible ",
+      "ansible-pull -i localhost, -U https://github.com/vjsmit/roboshop-ansible-1 roboshop.yml -e env=dev -e role_name=frontend",
+    ]
+  }
+
+}
+
+data "aws_ami" "example" {
+  owners = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
