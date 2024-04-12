f055954e24f097ea617521ccfd279f3a692e04b9
smvijay <vj.smit97@gmail.com>
2024-04-12 13:25:46 -0400

    resource

--- 06-module-1/main.tf
+++ 06-module-1/main.tf
@@ -8,3 +8,10 @@ module "sample2" {
   name = "sample2"
 }
 
+output "public_ip_sample1" {
+  value = module.sample1.public_ip
+}
+
+output "public_ip_sample2" {
+  value = module.sample2.public_ip
+}
