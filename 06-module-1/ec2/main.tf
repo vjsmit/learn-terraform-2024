75f3d4ff514178d54408e2d38a3b41f30d932c28
smvijay <vj.smit97@gmail.com>
2024-04-12 13:06:12 -0400

    resource

---
+++ 06-module-1/ec2/main.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sample.id]
+
+  tags = {
+    Name = var.name
+  }
+}
+
+data "aws_ami" "example" {
+  owners = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+resource "aws_security_group" "sample" {
+  name        = var.name
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress" {
+  security_group_id = aws_security_group.sample.id
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "egress" {
+  security_group_id = aws_security_group.sample.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+variable "name" {}
