e7a92095d5b815b699b736d90ea70e59883354b6
smvijay <vj.smit97@gmail.com>
2024-04-13 07:29:12 -0400

    count

--- 10-functions/main.tf
+++ 10-functions/main.tf
@@ -9,3 +9,11 @@ output "class" {
 output "class1" {
   value = length(var.class)
 }
+
+variable "fruits" {
+  default = [ "apple", "banana", "Guava" ]
+}
+
+output "fruits_count" {
+  value = length(var.fruits)
+}
