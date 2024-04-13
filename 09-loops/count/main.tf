f23349cc8ddcb2e4ed71c64a59436df8532a1f98
smvijay <vj.smit97@gmail.com>
2024-04-13 08:43:05 -0400

    count

--- 09-loops/count/main.tf
+++ 09-loops/count/main.tf
@@ -14,7 +14,8 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = var.instances[count.index]
+    #Name = var.instances[count.index]
+    Name = element(var.instances, count.index)
   }
 }
 data "aws_ami" "example" {
