6ee549afb628c2d6329af02955362f1e48559016
smvijay <vj.smit97@gmail.com>
2024-06-01 09:52:49 -0400

    tf output

--- 09-loops/count/main.tf
+++ 09-loops/count/main.tf
@@ -8,22 +8,38 @@
 #  }
 #}
 
+#resource "aws_instance" "web" {
+#  count         = length(var.instances)
+#  ami           = data.aws_ami.example.id
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    #Name = var.instances[count.index]
+#    Name = element(var.instances, count.index)
+#  }
+#}
+#data "aws_ami" "example" {
+#  owners = ["973714476881"]
+#  most_recent      = true
+#  name_regex       = "Centos-8-DevOps-Practice"
+#}
+#
+#variable "instances" {
+#  default = [ "frontend", "catalogue", "cart" ]
+#}
+
 resource "aws_instance" "web" {
-  count         = length(var.instances)
+  count = 3
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
 
   tags = {
-    #Name = var.instances[count.index]
-    Name = element(var.instances, count.index)
+    Name = "HelloWorld"
   }
 }
+
 data "aws_ami" "example" {
-  owners = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-}
-
-variable "instances" {
-  default = [ "frontend", "catalogue", "cart" ]
+  owners           = ["973714476881"]
 }
